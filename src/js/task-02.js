ea92ead40879aae0b28c8ded7b2dcc94872ee807
Polosmak Roman <polosmakrr@gmail.com>
2021-09-14 22:15:36 +0300

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+const listRef = document.querySelector(`#ingredients`);
+
+const elements = ingredients.map(element => {
+  const liEl = document.createElement('li');
+  liEl.textContent = element;
+
+  console.log(liEl);
+  return liEl;
+})
+
+listRef.append(...elements);
+
+
+
+
+
